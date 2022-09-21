28829a0a078c394f75e5724d508d376b47fc04d3
haramomoka <33130haramomoka@gmail.com>
2022-09-21 18:44:26 +0900

    update: ログイン画面作成しました

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,22 +1,30 @@
 /* Login.js */
 
 import React from 'react';
+import '../components/Login.css';
+import pic from '../img/loginmac.png';
 
 const Login = () => {
   return (
     <>
-      <h1>ログインページ</h1>
-      <form>
-        <div>
-          <label>メールアドレス</label>
+    <div className='container'>
+      <img src={pic} width={"250"} height={"250"} alt='macIcon'/>
+      <h1 className='login-page'>ログインページ</h1>
+      <form className='infobox'>
+        <div className='input'>
+          <label className='email-label'>メールアドレス</label>
           <input name='email' type='email' />
         </div>
-        <div>
-          <label>パスワード</label>
+        <div className='input'>
+          <label className='password-label'>パスワード</label>
           <input name='password' type='password' />
         </div>
-        <button>ログイン</button>
+        <div className='input'>
+          <button className='login-button'>ログイン</button>
+          <button className='register-button'>新規登録</button>
+          </div>
       </form>
+      </div>
     </>
   );
 };
