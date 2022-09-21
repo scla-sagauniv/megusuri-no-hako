08be15c441860aeded8e298eb4c7512072402be9
yunatoma <hautree0927@gmail.com>
2022-09-22 06:36:09 +0900

    fix: testコミット

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import Register from './pages/Register';
-import Login from './pages/Login';
-import Home from './pages/Home';
+// import Register from './pages/Register';
+// import Login from './pages/Login';
+// import Home from './pages/Home';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Test from './pages/Test';
 
@@ -8,9 +8,9 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={`/register/`} element={<Register />} />
+        {/* <Route path={`/register/`} element={<Register />} />
         <Route path={`/login/`} element={<Login />} />
-        <Route path={`/test`} element={<Home />} />
+        <Route path={`/test`} element={<Home />} /> */}
         <Route path='/' element={<Test />} />
       </Routes>
     </BrowserRouter>
--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { getAuth } from 'firebase/auth';
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+// import { getAuth } from 'firebase/auth';
+// import { initializeApp } from 'firebase/app';
+// import { getFirestore } from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,9 +15,9 @@ export const firebaseConfig = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
 
-export const auth = getAuth(app);
+// export const auth = getAuth(app);
 
-export const db = getFirestore(app);
+// export const db = getFirestore(app);
