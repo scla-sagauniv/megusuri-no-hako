4d5f0319b76041f9e0d50f432b631a4934959b42
yunatoma <hautree0927@gmail.com>
2022-09-22 06:18:46 +0900

    fix: testコミット

--- src/FirebaseConfig.js
+++ src/FirebaseConfig.js
@@ -6,7 +6,7 @@ import { getFirestore } from 'firebase/firestore';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
+export const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
