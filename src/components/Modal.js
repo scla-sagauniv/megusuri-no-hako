7fdb123d50332960e013276d13da11c0ea38fe82
shunsuke-kawata <kwt20238120@gmail.com>
2022-09-21 11:14:20 +0900

    フォームの処理を修正

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 
 
@@ -7,51 +8,66 @@ const Modal = (props) => {
     const closeModal = () => {
         props.setShowModal(false);
       };
+
+      const {
+        register,
+        handleSubmit,reset,
+        formState: { errors },
+      } = useForm();
+    
+      const onSubmit = (data) => {
+        console.log(data);
+        
+        closeModal()
+        reset()
+      }
+
   return (
     <>
-      {props.showFlag ? ( // showFlagがtrueだったらModalを表示する
-        <div id="overlay">
+      {props.showFlag ? ( 
+        // showFlagがtrueだったらformを表示する
+        <div id ="overlay" >
           <div id="modalContent">
-
-            <form id="task_form">
-
+            <form onSubmit={handleSubmit(onSubmit)}>
                 <div className="setting_box">
-                    <input id="task_title" type="text" placeholder="タイトルを入力"></input>
+                  <input id="task_title" type="text" placeholder="タイトルを入力" {...register("task_title")}></input>
                 </div>
+                
 
                 <div className="setting_box">
                     <label className="settings_label">優先度</label>
-                    <select id="task_priority" name="color">
+                    <select id="task_priority" {...register("task_priority")}>
                         <option value="最重要">最重要</option>
                         <option value="重要">重要</option>
                         <option value="中">中</option>
                         <option value="低">低</option>
                     </select>
                 </div>
-
-                <div className="setting_box">
+                <div className="setting_box" >
                     <label className="settings_label">期限を指定</label>
-                    <input id="task_deadline" type="date"></input>
+                    <input id="task_deadline" type="date" {...register("task_deadline")}></input>
                 </div>
-
                 <div className="setting_box">
                     <label className="settings_label">タスクの説明</label>
-                    <input id="task_description" type="text"></input>
+                    <input id="task_description" type="text" {...register("task_description")}></input>
                 </div>
-                
-                <button onClick={closeModal}>保存</button>
-            </form>
+              <input id="submit_btn" type="submit"/>
 
-            
-            
-        
+
+        </form>
+
           </div>
         </div>
+        
+       
+        
+        
       ) : (
         <></>// showFlagがfalseの場合はModalは表示しない
       )}
     </>
-  );
+   
+  )
 };
 
 export default Modal;
