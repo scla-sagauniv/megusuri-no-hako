09c7ebabd7dbc639f5b730c313b8dc6f7e198512
ryutaro-kimura <sasuker2264@gmail.com>
2022-09-21 10:08:40 +0900

    add: ホーム画面、ログイン、新規登録画面の追加

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import './App.css';
-import Main from './components/Main';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div style={{ padding: '50px' }}>
-      <h1 style={{ merginBottom: '20px' }}>タスク管理アプリ</h1>
-      <Main />
+    <div className='container'>
+      <BrowserRouter>
+        <Routes>
+          <Route path={`/register/`} element={<Register />} />
+          <Route path={`/login/`} element={<Login />} />
+          <Route path={`/`} element={<Home />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Main from '../components/Main';
+
+const Home = () => {
+  return (
+    <div style={{ padding: '50px' }}>
+      <h1 style={{ merginBottom: '20px' }}>タスク管理アプリ</h1>
+      <Main />
+    </div>
+  );
+};
+
+export default Home;
